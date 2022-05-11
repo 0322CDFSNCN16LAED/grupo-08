c57f28529c1063f2cadd0e2e1779d8ade2091460
parnorma <parnorma@gmail.com>
2022-05-11 15:55:31 -0300

    Estoy creando la estructura inicial del proyecto - Norma

---
+++ app.js
@@ -0,0 +1,16 @@
+const path = require("path");
+const express = require("express");
+const app = express();
+
+const PORT = 3005;
+
+app.listen(PORT, () => {
+    console.log("Servidor activo "+PORT);
+});
+
+app.use(express.static(path.join(__dirname,"public")));
+
+app.get('/',(req,res)=>{
+    //funcion controladora
+    res.sendFile(path.join(__dirname,"views/home.html"));
+});
