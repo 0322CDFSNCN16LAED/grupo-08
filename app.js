ea52fffc51beb0f0a80ead2e6d9fc909eede88b8
Mariana Candia <candia.marian@gmail.com>
2022-05-11 17:05:28 -0300

    Le agregue la estructura de carpetas y el arch normalize

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = 3005;
 
 app.listen(PORT, () => {
-    console.log("Servidor activo "+PORT);
+    console.log("Servidor activo en puerto "+PORT);
 });
 
 app.use(express.static(path.join(__dirname,"public")));
