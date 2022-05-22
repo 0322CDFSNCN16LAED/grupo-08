161e79c4a3d425ddea2b6889fec119150c0ff586
parnorma <parnorma@gmail.com>
2022-05-22 13:31:19 -0300

    creando la pagina de Login, agregados algunas clases en utils y theme y modificado app.js

--- app.js
+++ app.js
@@ -5,36 +5,40 @@ const app = express();
 const PORT = 3005;
 
 app.listen(PORT, () => {
-    console.log("Servidor activo en puerto "+PORT);
+  console.log("Servidor activo en puerto " + PORT);
 });
 
-app.use(express.static(path.join(__dirname,"public")));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/',(req,res)=>{
-    //funcion controladora
-    res.sendFile(path.join(__dirname,"views/home.html"));
-});
-
-app.get('/register',(req,res)=>{
-    res.sendFile(path.join(__dirname,"views/register.html"));
+app.get("/", (req, res) => {
+  //funcion controladora
+  res.sendFile(path.join(__dirname, "views/home.html"));
 });
 
-app.get('/carritocompras',(req,res)=>{
-    res.sendFile(path.join(__dirname,"views/carritocompras.html"));
+app.get("/register", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/register.html"));
 });
 
-app.get('/producto',(req,res)=>{
-    res.sendFile(path.join(__dirname,"views/producto.html"));
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/login.html"));
 });
-app.get('/producto-lamp',(req,res)=>{
-    res.sendFile(path.join(__dirname,"views/producto-lamp.html"));
+
+app.get("/carritocompras", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/carritocompras.html"));
 });
-app.get('/producto-silla',(req,res)=>{
-    res.sendFile(path.join(__dirname,"views/producto-silla.html"));
+
+app.get("/producto", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/producto.html"));
 });
-app.get('/catalogo',(req,res)=>{
-    res.sendFile(path.join(__dirname,"views/catalogo.html"));
+app.get("/producto-lamp", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/producto-lamp.html"));
 });
-app.get('/catalogo2',(req,res)=>{
-    res.sendFile(path.join(__dirname,"views/catalogo2.html"));
+app.get("/producto-silla", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/producto-silla.html"));
+});
+app.get("/catalogo", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/catalogo.html"));
+});
+app.get("/catalogo2", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/catalogo2.html"));
 });
