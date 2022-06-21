09a28898742b7ca55422373ac4de984f42c87da7
parnorma <parnorma@gmail.com>
2022-06-21 14:23:48 -0300

    agrego detalle del producto, modifique columnas en editar producto, agregue boton eliminar producto

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -1,42 +1,52 @@
 const db = require("../data/db-products");
 const path = require("path");
 
-const products = db.getAll();
+let products = db.getAll();
 
 module.exports = {
+  // ver todos los productos
   index: (req, res) => {
+    products = db.getAll();
     res.render("products/products", { productos: products });
   },
+  //ver el detalle de cada producto
   detail: (req, res) => {
-    res.send("vista para mostrar el detalle del producto");
+    res.render("products/details", { producto: db.getOne(req.params.id) });
   },
+  //crear un nuevo producto
   create: (req, res) => {
     res.send("vista para crear producto");
   },
+  //accion de procesar el producto. CREAR
   store: (req, res) => {
     res.send("procesa el crear producto");
   },
+  // vista para editar detalles de productos
   edit: (req, res) => {
-    let id = req.params.id
-    let productEdit = products.find(productos => productos.id == id)
-    res.render("products/productos-edit-product", {productoEditar: productEdit})
+    let id = req.params.id;
+    let productEdit = products.find((productos) => productos.id == id);
+    res.render("products/productos-edit-product", {
+      productoEditar: productEdit,
+    });
   },
+  // accion de actualizar un producto.
   update: (req, res) => {
-    const productEditIndex = db.getAll(req.params.id)
+    const productEditIndex = db.getAll(req.params.id);
 
-    products.splice(productEditIndex, 1, req.body)
+    products.splice(productEditIndex, 1, req.body);
 
-    db.saveAll(products)
-      res.send("vengo a moelstar");
-   // res.redirect("/products");
+    db.saveAll(products);
+    res.send("vengo a moelstar");
+    // res.redirect("/products");
   },
+  // accion de eliminar un producto
   destroy: (req, res) => {
-    const filterProductos = products.filter((producto)=>{
-      return producto.id != req.params.id
-    })
+    const filterProductos = products.filter((producto) => {
+      return producto.id != req.params.id;
+    });
 
-    db.saveAll(filterProductos)
-    
+    db.saveAll(filterProductos);
+
     res.redirect("/products");
   },
 };
