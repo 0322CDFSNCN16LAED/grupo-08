6ce1f1422977b817957b0b4500e1322f4d0ec521
Mariana Candia <candia.marian@gmail.com>
2022-06-21 11:42:33 -0300

    ruta post de register de usuarios

--- routers/user-router.js
+++ routers/user-router.js
@@ -2,8 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 const usersControllers = require("../controllers/usersControllers");
+
 /*Aca  los enrutadores*/
 router.get("/login", usersControllers.login);
+ 
 router.get("/register", usersControllers.register);
+router.post("/register", usersControllers.register);
 
 module.exports = router;
