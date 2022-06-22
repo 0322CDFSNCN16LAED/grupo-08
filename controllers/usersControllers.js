e06d6570ffa1f32a6daf8f99403bfd2b990c4343
Mariana Candia <candia.marian@gmail.com>
2022-06-22 14:04:05 -0300

    enrutador users

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -1,10 +1,29 @@
 const path = require("path");
+const db = require("../data/db-users");
 
 module.exports = {
   login: function (req, res) {
     res.render("users/login");
   },
+  showRegister: function(){
+
+  },
   register: function (req, res) {
     res.render("users/register");
+  },
+  index: function(){
+    
+  },
+  detail: function(){
+    
+  },
+  edit: function(){
+    
+  },
+  update: function(){
+    
+  },
+  delete: function(){
+    
   },
 };
