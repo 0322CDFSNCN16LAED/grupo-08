40f569b95402351ac435500d3e9fd4cf93543a75
parnorma <parnorma@gmail.com>
2022-06-22 19:37:46 -0300

    instalación de multer y config en el rputer de productos

--- routers/products-router.js
+++ routers/products-router.js
@@ -1,14 +1,27 @@
 const express = require("express");
 const router = express.Router();
+const multer = require("multer");
+const path = require("path");
 
 const productControllers = require("../controllers/productControllers");
+
+const multerDiskStorage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, "../../public/images/products"));
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
+
+const uploadFile = multer({ storage: multerDiskStorage });
 
 /* Listado de productos*/
 router.get("/", productControllers.index);
 
 /* Crear un producto*/
 router.get("/create", productControllers.create);
-router.post("/", productControllers.store);
+router.post("/", uploadFile.single("image"), productControllers.store);
 
 /* Ver detalle de un producto*/
 router.get("/:id", productControllers.detail);
@@ -21,4 +34,3 @@ router.put("/:id", productControllers.update);
 router.delete("/:id", productControllers.destroy);
 
 module.exports = router;
-    
