d18b2760793291014578f872109bbac224704518
parnorma <parnorma@gmail.com>
2022-07-13 11:31:24 -0300

    user controllers

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -27,10 +27,8 @@ module.exports = {
         //comparo la contraseña
         delete userToLogin.password; //por seguridad la borramos
         req.session.userLogged = userToLogin; // creamos la variable en session con el usuario loggeado
-        console.log("usuario logueado" + req.session.userLogged);
         // si el usuario tildó ser recordado:
         if (req.body.recordar) {
-          console.log("esta entrando en req.body.recordar");
           res.cookie("userEmail", req.body.email, { maxAge: 1000 * 60 }); //dura 1 minuto
         }
         return res.redirect("/users/" + userToLogin.id); //¿va en esta parte + userToLogin.id?
@@ -120,21 +118,19 @@ module.exports = {
   },
   //ver datalle de cada usuario.
   detail: function (req, res) {
-    //return res.send("estando en detalle " + req.cookies.userEmail);
-    console.log("detalle" + req.cookies.userEmail);
     res.render("../views/users/user-detail", {
       user: db.getOne(req.params.id),
     });
   },
-  edit: function(req, res){    
-    // Muestra formulario de edicion de usuario               
-    const userToEdit =  db.getOne(req.params.id);        
-    res.render('users/edit-user', { userToEdit: userToEdit });
+  edit: function (req, res) {
+    // Muestra formulario de edicion de usuario
+    const userToEdit = db.getOne(req.params.id);
+    res.render("users/edit-user", { userToEdit: userToEdit });
   },
   update: (req, res) => {
-    res.send("pagina en rehabilitacion")
-  }
-/*  update: (req, res) => {
+    res.send("pagina en rehabilitacion");
+  },
+  /*  update: (req, res) => {
     // Metodo para actualizar info del usuario
     const validationErrors = validationResult(req) // guardo los errores de validacion
     if (!validationErrors.isEmpty()) {
