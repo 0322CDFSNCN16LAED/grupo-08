4e75879ada64016c6c4b2d9014a888bbd0a34897
Mariana Candia <candia.marian@gmail.com>
2022-08-10 20:44:00 -0300

    trabajando en R de CRUD

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -1,8 +1,9 @@
 const bcryptjs = require("bcryptjs"); //Requerimos el encriptador
 const db = require("../data/db-users")//DBJSON
-const database = require("../database/models"); //Requerimos la DB de usuarios
+const database = require("../database/models"); //Requerimos la DB de Sequelize
 
 const { validationResult } = require("express-validator");
+
 
 module.exports = {
   login: function (req, res) {
@@ -62,9 +63,10 @@ module.exports = {
     let userRoles = await database.UserRole.findAll({raw:true, nest: true});
       return res.render("users/register", {userRoles});
   },
+  // Metodo que procesar el Registro de usuario nuevo (POST)
   register: async function (req, res) {
-    let userRoles = await database.UserRole.findAll({raw:true, nest: true});
-    // Metodo que procesar el Registro de usuario nuevo (POST)
+    let userRoles = await database.UserRole.findAll({raw:true, nest: true});//traigo el modelo de userRoles
+    
     const validationErrors = validationResult(req); // guardo los errores de validacion
     if (!validationErrors.isEmpty()) {
       // SI HAY ERRORES, renderizo el formulario
@@ -113,22 +115,85 @@ module.exports = {
             
       })
         console.log(req.body)
-        res.redirect("/");
+        res.redirect("/"); //finalmente, redirecciono al home.
       }
     }
   },
+  //READ - listar todos los usuarios
+  index: async function (req, res){
+    try {
+    let userRoles = await database.UserRole.findAll({raw:true, nest: true});//traigo el modelo de userRoles
+    let address = await database.Address.findAll({raw:true, nest: true});
+    let orders = await database.Orders.findAll({raw:true, nest: true});
+    let users = await database.User.findAll({
+      include: ['userRoles', 'address', 'orders']
+    })} catch (error){
+      console.error(error)
+  } res.render("../views/users/user-detail", {users})
+    console.log(users)
+  },
+   
+  
+  // detalle de user
+  detail: async function (req, res) {
+    let userRoles = await database.UserRole.findAll({raw:true, nest: true});//traigo el modelo de userRoles
+    let address = await database.Address.findAll();
+    let orders = await database.Orders.findAll();
+    let user = await database.User.findByPK(req.params.id, {
+      include: ['userRoles', 'address', 'orders']
+    })
+    res.render("../views/users/user-detail", {user})
+    console.log(user)
+  }
+    
+
+
+  //create user CRUD metodo JSON 
+  // SI NO HAY USUARIO CON ESE MAIL EN LA DB - LO GUARDO
+
+/*  let users = db.getAll(); //traigo todos los usuarios
+  const newUser = {
+    // guardamos un nuevo usuario con los datos del req
+    nombre: req.body.nombre,
+    apellido: req.body.apellido,
+    name: req.body.name,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    telefono: req.body.telefono,
+    direccion: req.body.direccion,
+    phoneNumber: req.body.phoneNumber,
+    address: req.body.address,
+    city: req.body.city,
+    state: req.body.state,
+    country: req.body.country,
+    zipCode: req.body.zipCode,
+    password: bcryptjs.hashSync(req.body.password, 10), // encriptamos la password
+    profile: req.file ? req.file.filename : "defaultImage.jpg",
+  };
+  if (users.length) {
+    // hacemos un nuevo nro de id de usuaro
+    newUser.id = users[users.length - 1].id + 1;
+  } else {
+    newUser.id = 1;
+  }
+  users.push(newUser); //pusheamos en el array el nuevo usuario
+  db.saveAll(users); // metodo guardar que sobreescribe la db
+  res.redirect("/users");
+}
+}
+},
 
   //vista de todos los usuarios.
   index: function (req, res) {
     let users = db.getAll();
     res.render("users/index", { users: users });
   },
-  //ver datalle de cada usuario.
-  detail: function (req, res) {
+  //ver datalle de cada usuario 
+ /* detail: function (req, res) {
     res.render("../views/users/user-detail", {
       user: db.getOne(req.params.id),
     });
-  },
+  }*/,
   edit: function (req, res) {
     // Muestra formulario de edicion de usuario
     const userToEdit = db.getOne(req.params.id);
