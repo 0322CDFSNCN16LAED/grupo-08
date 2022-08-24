af41e006f7de1dbd5ec4ffc2e371a6502221c7c1
nahuelblanco <blanconahuel97@gmail.com>
2022-08-24 17:39:08 -0300

    register validacion

--- public/js/js-front.js
+++ public/js/js-front.js
@@ -32,15 +32,32 @@ window.addEventListener("load", function(){
 
         let errores = [];
 
+        let nameRegister = document.querySelector("input.name");
+
+        if(nameRegister.value == ""){
+            errores.push("Debe ingresar un e-mail");
+        }
+
+        let lastNameRegister = document.querySelector("input.lastname");
+
+        if(lastNameRegister.value == ""){
+            errores.push("Debe ingresar un e-mail");
+        }
+
         let inputEmailRegister = document.querySelector("input.email");
 
         if(inputEmailRegister.value == ""){
             errores.push("Debe ingresar un e-mail");
         }
 
-        let inputPassword = document.querySelector("input.pass");
+        let inputPassword = document.querySelector("input.password");
         if(inputPassword.value == "" ){
             errores.push("Debe ingresar una contraseña");
+        }
+
+        let inputProfilePic = document.querySelector("input.profilePic");
+        if(inputProfilePic.value == "" ){
+            errores.push("Debe seleccionar una imagen de perfil");
         }
 
         if(errores.length > 0) {
