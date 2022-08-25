f1bf4b46345a76c3617e0c8a7b7f4eb07ac17066
Mariana Candia <candia.marian@gmail.com>
2022-08-25 10:26:50 -0300

    cambios en modelos

--- database/config/config.js
+++ database/config/config.js
@@ -2,7 +2,7 @@ module.exports = {
   development: {
     username: "root",
     password: "",
-    port: "3307",
+    port: "3306",
     database: "decohome",
     host: "127.0.0.1",
     dialect: "mysql",
