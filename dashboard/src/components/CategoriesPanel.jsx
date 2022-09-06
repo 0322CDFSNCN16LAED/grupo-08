566ca51ef645110e4284dd0e74f8b30f6c1c8982
Mariana Candia <candia.marian@gmail.com>
2022-09-06 20:39:51 -0300

    pieChart funcionando

--- dashboard/src/components/CategoriesPanel.jsx
+++ dashboard/src/components/CategoriesPanel.jsx
@@ -1,29 +1,22 @@
-import React from "react";
-import { Pie } from "react-chartjs-2";
-import { categoriesInfo  } from "../consts/categoriesInfo";
-import { categoriesPieChart } from '../consts/categoriesPieChart';
+import React from 'react';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Pie } from 'react-chartjs-2';
+import { categoriesInfo } from '../consts/categoriesInfo'
+ChartJS.register(ArcElement, Tooltip, Legend);
 
+export const data = {
+    labels: categoriesInfo.map((c) => (c.name)),
+    datasets: [
+        {
+            label: '# of Votes',
+            data:  categoriesInfo.map((c) => (c.cantidad)),
+            backgroundColor: categoriesInfo.map((c) => (c.backgroundColor)),
+            borderColor: categoriesInfo.map((c) => (c.borderColor)),
+            borderWidth: 1,
+        },
+    ],
+};
 
-export default function CategoriesPanel({
-    labels = [categoriesInfo.map((c) => (c.name))],
-    datasets = [{
-        data: [categoriesPieChart.map((c) => (c.data))],
-        backgroundColor : [categoriesPieChart.map((c) => (c.backgroundColor))]
-    }]
-}) {
-  return (
-    <div>
-    <h4 style={{color: 'green'}}> Soy el componente PANEL DE CATEGORIAS </h4>
-    <Pie
-      options={{
-        width: "400",
-        height: "400"
-      }}
-      data={{
-        labels: labels,
-        datasets: datasets
-      }}
-    /> 
-    </div>
-  );        
+export default function CategoriesPanel() {
+    return <Pie data={data} />;
 }
--- dashboard/src/consts/categoriesInfo.js
+++ dashboard/src/consts/categoriesInfo.js
@@ -5,35 +5,48 @@ export const categoriesInfo = [
       id: 1,
       name: "Muebles",
       icon: <i className="fas fa-chair"></i>,
-      cantidad: 154
+      cantidad: 1540,
+      backgroundColor: 'rgba(255, 99, 132, 0.2)',
+      borderColor: 'rgba(255, 99, 132, 1)'
     },
     {
       id: 2,
       name: "Textiles",
       icon: <i className="far fa-blanket"></i>,
-      cantidad: 154
+      cantidad: 800,
+      backgroundColor: 'rgba(54, 162, 235, 0.2)',
+      borderColor: 'rgba(54, 162, 235, 1)'
     },
     {
       id: 3,
       name: "Vajilla", 
       icon: <i className="fa-regular fa-champagne-glasses"></i>,
-      cantidad: 154
+      cantidad: 7754,
+      backgroundColor: 'rgba(255, 206, 86, 0.2)' ,
+      borderColor: 'rgba(255, 206, 86, 1)'
     },
     {
       id: 4,
       name: "Accesorios", 
       icon: <i className="fas fa-icons"></i>,
-      cantidad: 154
+      cantidad: 4500,
+      backgroundColor:'rgba(75, 192, 192, 0.2)' ,
+      borderColor: 'rgba(75, 192, 192, 1)'
     },
     {
       id: 5,
       name: "Iluminación", 
       icon: <i className="far fa-lamp-desk"></i>,
-      cantidad: 154
+      cantidad: 2504,
+      backgroundColor: 'rgba(153, 102, 255, 0.2)',
+      borderColor: 'rgba(153, 102, 255, 1)'
     },
     {
       id: 6,
       name: "Otros",
-      icon: <i className="fas fa-box-full"></i> 
+      icon: <i className="fas fa-box-full"></i> ,
+      cantidad: 8500,
+      backgroundColor: 'rgba(255, 159, 64, 0.2)',
+      borderColor: 'rgba(255, 159, 64, 1)'
     }
   ]
