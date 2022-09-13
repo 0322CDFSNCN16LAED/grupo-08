473edb53e2f15716af4694e4cddf3ac2b4e5697a
Camila Mendieta <camilaa.mendieta@gmail.com>
2022-09-13 00:03:14 -0300

    vfno

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -113,7 +113,7 @@ module.exports = {
     /*const productEdit = await db.Product.findByPk(req.params.id, {
       include: ["Rooms"],
     });*/
-    const productEdit = await db.Product.findOne({
+    const productoEditar = await db.Product.findOne({
       where: { id: req.params.id },
       include: ["Rooms"],
       paranoid: true,
@@ -134,7 +134,7 @@ module.exports = {
     //res.send(productEdit);
     res.render("products/productos-edit-product", {
       
-      productoEditar: productEdit,
+      productoEditar: productoEditar,
       vInstallments: vInstallments,
       vCategorys: vCategorys,
       vRooms: vRooms,
@@ -148,21 +148,19 @@ module.exports = {
     let productId = req.params.id;
 
     const resultValidation = validationResult(req);
-  
-    //const oldProduct = db.Product.findByPk(productoId);
-
-    const oldProduct = db.Product.findByPk(req.params.id);
-    const productEdit = await db.Product.findOne({
+ //   const oldProduct = db.Product.findByPk(req.params.id);
+    
+    /*const productEdit = await db.Product.findOne({
       where: { id: req.params.id },
       include: ["Rooms"],
       paranoid: true,
-    });
+    });*/
     /*const vInstallments = req.body.installmentId
     const vCategorys = req.body.categoryId 
     const vRooms = req.body.rooms
     const vStyles = req.body.styleId  
     const vColours = req.body.colourId   
-    const vBrands = req.body.brandId */
+    const vBrands = req.body.brandId 
 
     const vInstallments = await db.Installment.findAll({order: [["name", "asc"]],});
     const vCategorys = await db.Category.findAll({ order: [["name", "asc"]] });
@@ -171,7 +169,7 @@ module.exports = {
     const vColours = await db.Colour.findAll({ order: [["name", "asc"]] });
     const vBrands = await db.Brand.findAll({ order: [["name", "asc"]] });
   
-    /*const vInstallments = db.Installment.findByPk(req.params.id);
+    const vInstallments = db.Installment.findByPk(req.params.id);
     const vCategorys = db.Category.findByPk(req.params.id);
     const vRooms = db.Room.findByPk(req.params.id);
     const vStyles = db.Style.findByPk(req.params.id);
@@ -179,7 +177,7 @@ module.exports = {
     const vBrands = db.Brand.findByPk(req.params.id);
     res.send(req.body);*/
 
-    let ambientes = [];
+   /* let ambientes = [];
     if (req.body.rooms) {
       if (typeof req.body.rooms == "string") {
         ambientes.push(req.body.rooms);
@@ -187,9 +185,9 @@ module.exports = {
         ambientes = req.body.rooms;
       }
     }
-    req.body.rooms = ambientes;
+    req.body.rooms = ambientes;*/
 
-    if (resultValidation.errors) {
+    /*if (resultValidation.errors) {
       //res.send(req.body);
       return res.render("products/productos-edit-product", {
         errors: resultValidation.mapped(), //convierte al array en un obj literal
@@ -201,8 +199,33 @@ module.exports = {
         vRooms,
         vColours,
         vBrands,
+      })
+    } else {*/
+
+    // OTRA VERSION DEL EDIT PRODUCTOOOOOOO---------//
+    let rooms = await db.Room.findAll()
+    const oldProduct = db.Product.findByPk(req.params.id, 
+      {include: ["Category", "Colour", "Brand", "Installment", "Style"],
+  });
+    //capturo el registro a modificar
+    let product = await db.Product.findByPk(req.params.id, {
+      include: ["Category", "Colour", "Brand", "Installment", "Style"]
+    });
+    // verifico
+    if (!resultValidation.isEmpty()) {
+      const voldData = {
+        ...req.body,
+        id: req.params.id,
+        profilePic: "defaultImage.jpg",
+      };
+      res.render("products/productos-edit-product", {
+        //renderizo el formulario
+        errors: resultValidation.mapped(), // con los errores mappeados y
+        oldData: voldData, // los datos que sí pasaron la validacion
+        rooms,
       });
     } else {
+      // si no hay errores
       try {
         let resp = await db.Product.update(
           {
@@ -225,9 +248,6 @@ module.exports = {
             } else {
               ambientes = req.body.rooms;
             }
-          }
-        }
-
         await db.RoomProduct.destroy({ where: { productId: productId } });
         /*if (ambientes.length > 0) {
         ambientes.forEach(async (element) => {
@@ -238,12 +258,14 @@ module.exports = {
         });
       }*/
         res.redirect("/products");
-      } catch (error) {
+      } 
+    } 
+  }
+    catch (error) {
         res.send("aca hay un error  " + error);
       }
     }
-  },
-  // accion de eliminar un producto
+    },  // accion de eliminar un producto
   destroy: async (req, res) => {
     const productoId = req.params.id;
     try {
@@ -282,3 +304,5 @@ module.exports = {
     }
   },
 };
+
+
