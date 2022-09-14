61bef18ff1acdd7dc8969c2430e06f9a06abb20e
parnorma <parnorma@gmail.com>
2022-09-14 12:14:51 -0300

    ocmit

--- dashboard/src/components/LastUserRegister.jsx
+++ dashboard/src/components/LastUserRegister.jsx
@@ -1,9 +1,16 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
+import React from "react";
+import { useState, useEffect } from "react";
 
-import { styled, Card, CardHeader, CardMedia, CardContent, CardActions } from '@mui/material';
-import { Collapse, Avatar, IconButton, Typography } from '@mui/material';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import {
+  styled,
+  Card,
+  CardHeader,
+  CardMedia,
+  CardContent,
+  CardActions,
+} from "@mui/material";
+import { Collapse, Avatar, IconButton, Typography } from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 /// PROBLEMA: CÒMO ERA EL TEMA DE LAS IMAGENES Y AS URL ESTATICAS?
 // PROBLEMA: FORMATO FECHA CREATEDAT ESTA FEO
@@ -14,9 +21,9 @@ const ExpandMore = styled((props) => {
   const { expand, ...other } = props;
   return <IconButton {...other} />;
 })(({ theme, expand }) => ({
-  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
-  marginLeft: 'auto',
-  transition: theme.transitions.create('transform', {
+  transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
+  marginLeft: "auto",
+  transition: theme.transitions.create("transform", {
     duration: theme.transitions.duration.shortest,
   }),
 }));
@@ -31,42 +38,49 @@ export default function LastUserRegister() {
   const [last, setLast] = useState(null);
 
   useEffect(() => {
-    console.log ('%cSe montó comp LastUserRegister', 'color: green')
+    console.log("%cSe montó comp LastUserRegister", "color: green");
     fetch(`${EXPRESS_HOST}/api/users/lastuserregistered`)
-      .then(response => response.json())
-      .then(data => {
-        setLast (data);        
+      .then((response) => response.json())
+      .then((data) => {
+        setLast(data);
       })
-      .catch(error => console.error (error));    
+      .catch((error) => console.error(error));
   }, []);
 
-  useEffect(()=> {
-    console.log('%cSe actualizó el comp LastUserRegister', 'color: yellow', {last});
-}, [last])
+  useEffect(() => {
+    console.log("%cSe actualizó el comp LastUserRegister", "color: yellow", {
+      last,
+    });
+  }, [last]);
 
   return (
     <Card sx={{ maxWidth: 345 }}>
-      {!last ? <p> Cargando...</p> :
-      <CardHeader
-        avatar={
-          <Avatar sx={{ bgcolor: '#d56b27' }} aria-label="recipe">
-            P
-          </Avatar>
-        }
-        title="ULTIMO USUARIO REGISTRADO"
-        subheader={last.datavalue.createdAt}
-      />}
-      {!last ? <p> Cargando...</p> :
-      <CardMedia
-        component="img"
-        height="194"
-        image={last.datavalue.profilePic}
-        alt="Imagen del usuario o producto"
-      />}
+      {!last ? (
+        <p> Cargando...</p>
+      ) : (
+        <CardHeader
+          avatar={
+            <Avatar sx={{ bgcolor: "#d56b27" }} aria-label="recipe">
+              P
+            </Avatar>
+          }
+          title="ULTIMO USUARIO REGISTRADO"
+          subheader={last.datavalue.createdAt}
+        />
+      )}
+      {!last ? (
+        <p> Cargando...</p>
+      ) : (
+        <CardMedia
+          component="img"
+          height="194"
+          image={last.datavalue.profilePic}
+          alt="Imagen del usuario o producto"
+        />
+      )}
       <CardContent>
-        <Typography variant="h5" >
-          {!last ? <p> Cargando...</p> 
-          : last.datavalue.email   }
+        <Typography variant="h5">
+          {!last ? <p> Cargando...</p> : last.datavalue.email}
         </Typography>
       </CardContent>
       <CardActions disableSpacing>
@@ -80,58 +94,59 @@ export default function LastUserRegister() {
         </ExpandMore>
       </CardActions>
       <Collapse in={expanded} timeout="auto" unmountOnExit>
-          {!last ? <p> Cargando...</p> :        
+        {!last ? (
+          <p> Cargando...</p>
+        ) : (
           <CardContent>
-          <Typography variant="h6">Información detallada:</Typography>
-          <Typography paragraph>
-            Nombre :{last.datavalue.name }
-            </Typography>
-          <Typography paragraph>
-            Apellido :{last.datavalue.lastname } 
-          </Typography>
-          <Typography paragraph>
-            Rol de Usuario :  {last.datavalue.userRole.name }
+            <Typography variant="h6">Información detallada:</Typography>
+            <Typography paragraph>Nombre :{last.datavalue.name}</Typography>
+            <Typography paragraph>
+              Apellido :{last.datavalue.lastname}
             </Typography>
             <Typography paragraph>
-            Teléfono :  {last.datavalue.phoneNumber }
+              Rol de Usuario : {last.datavalue.userRole.name}
             </Typography>
-            {!last.datavalue.address ?
             <Typography paragraph>
-            Dirección :  Sin datos
+              Teléfono : {last.datavalue.phoneNumber}
             </Typography>
-            :  <Typography paragraph>
-            Dirección :  {last.datavalue.address.address}
-            </Typography>}
-            {!last.datavalue.address ?
-            <Typography paragraph>
-            Ciudad :  Sin datos
-            </Typography>
-            :  <Typography paragraph>
-            Ciudad :  {last.datavalue.address.city}
-            </Typography>}
-            {!last.datavalue.address ?
-            <Typography paragraph>
-            Provincia :  Sin datos
-            </Typography>
-            :  <Typography paragraph>
-            Provincia :  {last.datavalue.address.state}
-            </Typography>}
-            
-            {!last.datavalue.address ?
-            <Typography paragraph>
-            País :  Sin datos
-            </Typography>
-            :  <Typography paragraph>
-            País :  {last.datavalue.address.country}
-            </Typography>}
-            {!last.datavalue.address ?
-            <Typography paragraph>
-            Código Postal :  Sin datos
-            </Typography>
-            :  <Typography paragraph>
-            Código Postal :  {last.datavalue.address.zipcode}
-            </Typography>}
-        </CardContent>}
+            {!last.datavalue.address ? (
+              <Typography paragraph>Dirección : Sin datos</Typography>
+            ) : (
+              <Typography paragraph>
+                Dirección : {last.datavalue.address.address}
+              </Typography>
+            )}
+            {!last.datavalue.address ? (
+              <Typography paragraph>Ciudad : Sin datos</Typography>
+            ) : (
+              <Typography paragraph>
+                Ciudad : {last.datavalue.address.city}
+              </Typography>
+            )}
+            {!last.datavalue.address ? (
+              <Typography paragraph>Provincia : Sin datos</Typography>
+            ) : (
+              <Typography paragraph>
+                Provincia : {last.datavalue.address.state}
+              </Typography>
+            )}
+
+            {!last.datavalue.address ? (
+              <Typography paragraph>País : Sin datos</Typography>
+            ) : (
+              <Typography paragraph>
+                País : {last.datavalue.address.country}
+              </Typography>
+            )}
+            {!last.datavalue.address ? (
+              <Typography paragraph>Código Postal : Sin datos</Typography>
+            ) : (
+              <Typography paragraph>
+                Código Postal : {last.datavalue.address.zipcode}
+              </Typography>
+            )}
+          </CardContent>
+        )}
       </Collapse>
     </Card>
   );
